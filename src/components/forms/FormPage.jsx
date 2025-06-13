bc396951770fa14dfe955c16f8354dc8f9954158
Tana Ofik <ofik.tana@gmail.com>
2025-06-13 15:39:57 +0100

    minor changes

--- src/components/forms/FormPage.jsx
+++ src/components/forms/FormPage.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 export default function FormPage({ children }) {
   return (
-    <div className="w-screen flex justify-center items-center py-12 px-4">
+    <div className="w-screen flex justify-center items-center py-12 px-2">
       { children }
     </div>
   )
